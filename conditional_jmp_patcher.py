1a473e8cebbfa082ac336c8bb00e35f0960d84fa
kvmc <kevin.cicoria@gmail.com>
2024-04-23 17:46:24 -0400

    forgot to define actual target

--- conditional_jmp_patcher.py
+++ conditional_jmp_patcher.py
@@ -10,11 +10,13 @@ def get_conditional_jumps():
 
 def fix_conditional_jumps(jmp_list):
 	for i in range(len(jmp_list)-1):
-		currinst,currinstlen = jmp_list[i][1],bv.get_instruction_length(jmp_list[i][1])
-		nextinst,nextinstlen = jmp_list[i+1][1],bv.get_instruction_length(jmp_list[i+1][1])
+		currinst,currinstlen = jmp_list[i][1]
+        currinstlen = bv.get_instruction_length(jmp_list[i][1])
+		nextinst = jmp_list[i+1][1]
 		
 		if  (currinst+currinstlen == nextinst) and (jmp_list[i][0][2] == jmp_list[i+1][0][2]):
-			print(f"Patching @ {jmp_list[i][1]}") 
+		    target = jmp_list[0][2]	
+            print(f"Patching @ {jmp_list[i][1]}") 
 			print(f"Match 1: {jmp_list[i][1]} -> {jmp_list[i][0]}")
 			print(f"Match 2: {jmp_list[i+1][1]} -> {jmp_list[i+1][0]}")
 			target = jmp_list[i][0][2]
